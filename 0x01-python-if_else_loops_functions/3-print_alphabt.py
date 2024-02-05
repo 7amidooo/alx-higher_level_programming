ceeb12aa80a7532d3fc805f3a64fbb134fb2ddbc
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-05 02:48:41 +0200

    all alphbet expet q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
+# print all alphabet expet q and e
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end='')
