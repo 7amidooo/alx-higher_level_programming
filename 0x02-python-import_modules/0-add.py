6956a2df43b5683e513a7cc86365b34bc62d679c
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-07 07:22:48 +0200

    acc

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,7 @@
 this is a program to print a + b in the direct mode only
 '''
 if __name__ == "__main__":
-    from 0-add import add
+    from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
