2ccef6ad772dba9053a2437a8f54f0894886f300
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-07 08:31:56 +0200

    Acc

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+'''
+program to print the summtion, multiplication, divition and substraction in the direct mode
+'''
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
