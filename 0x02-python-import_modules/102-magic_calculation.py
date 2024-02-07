f4632e232fa31f516b6dbdaa3024d0f9d3178afb
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-07 08:22:03 +0200

    Acc

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+dealing with bytecode
+'''
+
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if a < b:
+        ans = add(a, b)
+        for x in range(4, 6):
+            ans = add(ans, x)
+        return ans
+    else:
+        return (sub(a, b))
