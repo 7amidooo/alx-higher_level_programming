a136d19f1b6569717d2eb435796b09bba73445fd
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-24 03:27:51 +0200

    acc

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
 -- Sort elemnts 
--- Show them desc 
-SELECT score , name FROM second_table ORDER BY score DESC WHERE score >= 10;
+ELECT score , name FROM second_table WHERE score >= 10 ORDER BY score DESC;
