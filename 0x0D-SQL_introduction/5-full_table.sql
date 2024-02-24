3037900955ae7d57b6f1df6b6b1eef3b4c34c7eb
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-24 02:48:21 +0200

    acc

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
--- describe table 
-USE hbtn_0c_0;
-SHOW CREATE  
-TABLE 'first_table';
+-- Prints the full description of the table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
