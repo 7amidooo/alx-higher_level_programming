fa6a5fb48db01cb25bf84864e02fa7c05f646261
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-02-24 03:15:58 +0200

    acc

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Create a table
-create table second_table (id INT, name VARCHAR(265), score INT);
+create table IF NOT EXISTS second_table (id INT, name VARCHAR(265), score INT);
 INSERT INTO second_table VALUES (1, 'John', 10);
 INSERT INTO second_table VALUES (2, 'Alex', 3); 
 INSERT INTO second_table VALUES (3, 'Bob', 14); 
-INSERT INTO second_table VALUES (4, 'George, 8);
+INSERT INTO second_table VALUES (4, 'George, 8); 
