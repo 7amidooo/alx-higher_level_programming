dd81c5afba7342426ac2cb221b7dd3af039917e6
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-03-05 07:43:36 +0200

    Acc

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,6 +48,9 @@ class Square:
             print()
             return
 
+        for i in range(0, self.__position[1]):
+            print()
+
         for i in range(0, self.__size):
             for k in range(0, self.__position[0]):
                 print(" ", end='')
