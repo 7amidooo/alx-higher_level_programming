31d16988706ce0b2fc8b00c3b5ef82c90b7ba7a2
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-03-17 16:18:42 +0200

    acc

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  First using of mysqldb"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    arg = sys.argv
+    dp = MySQLdb.connect(host="localhost", user=arg[1],
+                         passwd=arg[2], db=arg[3], port=3306)
+    cur = dp.cursor()
+    cur.execute("select * from states where name like 'N%' ")
+    result = cur.fetchall()
+    for dps in result:
+        print(dps)
+    cur.close()
+    dp.close()
