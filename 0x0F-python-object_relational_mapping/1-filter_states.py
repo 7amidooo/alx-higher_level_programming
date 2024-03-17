7ddec71f0fadce3729127bdeb8b25b1f56c74f34
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-03-17 16:19:50 +0200

    acc

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     dp = MySQLdb.connect(host="localhost", user=arg[1],
                          passwd=arg[2], db=arg[3], port=3306)
     cur = dp.cursor()
-    cur.execute("select * from states where name like 'N%' ")
+    cur.execute("select * from states where name like 'N%' ORDER BY states.id")
     result = cur.fetchall()
     for dps in result:
         print(dps)
