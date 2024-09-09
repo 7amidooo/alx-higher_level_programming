3b46e195127d9dd5ce9a39b773e1561d1f99cf5b
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-09-09 12:55:05 +0300

    Avv

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,7 +7,7 @@ Function to list items of the class
 def lookup(obj):
     """
     Lookup(obj)
-    return: List of obj attributes and methods
+    Return: List of obj attributes and methods
     """
     obj1 = obj()
     return obj1.__dir__()
