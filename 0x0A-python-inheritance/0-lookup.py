11e02b03a28773cb3826ac69e7c71d83960beee3
abdelhamid2004 <abdelhamideslm900@gmail.com>
2024-09-09 12:53:35 +0300

    acc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,11 +3,11 @@
 Function to list items of the class
 """
 
-if __name__ == "__main__":
-    def lookup(obj):
-        """
-        Lookup(obj)
-        return: List of obj attributes and methods
-        """
-        obj1 = obj()
-        return obj1.__dir__()
+
+def lookup(obj):
+    """
+    Lookup(obj)
+    return: List of obj attributes and methods
+    """
+    obj1 = obj()
+    return obj1.__dir__()
